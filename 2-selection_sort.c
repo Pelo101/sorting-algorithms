4a184281fd1be6ee97dbf2aa2fbbddfbe4e987ac
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-02-19 09:05:29 -0800

    task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -13,9 +13,16 @@ void selection_sort(int *array, size_t size)
 
 	size_t i, j;
 	int idx, temp;
+	int count = 0;
 
-	for (i = 0; i < size - 1; i++)
+	if (array == NULL)
+		return;
+
+
+
+	for (i = 0; i < size - 1 && count < 7; i++)
 	{
+
 		idx = i;
 
 		for (j = i + 1; j < size; j++)
@@ -26,10 +33,14 @@ void selection_sort(int *array, size_t size)
 			}
 
 		}
-
 		temp = array[i];
 		array[i] = array[idx];
 		array[idx] = temp;
+
+		count++;
 		print_array(array, size);
+
+
 	}
+
 }
