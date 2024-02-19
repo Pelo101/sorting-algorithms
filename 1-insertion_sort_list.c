5bb1da53817d6ebe14892de6018d1350ad1b43f3
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-02-19 05:38:57 -0800

    task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,7 +6,7 @@
 /**
  *insertion_sort_list - function that sorts doubly linked list of integers
  *in ascending order using the insertion sort algorithm
- *list: doubly linked list
+ *@list: doubly linked list
  */
 void insertion_sort_list(listint_t **list)
 {
@@ -14,46 +14,40 @@ void insertion_sort_list(listint_t **list)
 	listint_t *temp;
 	listint_t *current = NULL;
 
-	if (*list == NULL || (*list)->next == NULL)	
+	if (*list == NULL || (*list)->next == NULL)
 	{
 		return;
 	}
 
 	curr_node = (*list)->next;
-
 	while (curr_node != NULL)
 	{
 		temp = curr_node;
 		current = curr_node->prev;
-	
+
 		while (current != NULL && current->n > temp->n)
 		{
-
 			if (current->prev != NULL)
 			{
-
 				current->prev->next = temp;
 			}
 			else
 			{
-
 				*list = temp;
 			}
 			if (temp->next != NULL)
 			{
 				temp->next->prev = current;
-
-			}	
-
+			}
 			current->next = temp->next;
 			temp->prev  = current->prev;
 			temp->next = current;
 			current->prev = temp;
 
 			current = temp->prev;
+			print_list(*list);
 		}
 		curr_node = curr_node->next;
-	}	
+	}
 	print_list(*list);
-
 }
