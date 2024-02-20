fdce41a04d5f4f8eb7dd65c7634109320631a28e
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-02-20 01:32:58 -0800

    header

--- sort.h
+++ sort.h
@@ -23,7 +23,7 @@ void selection_sort(int *array, size_t size);
 void swap(int *a, int *b);
 void quick_sort(int *array, size_t size);
 void quick_sortL(int arr[], int low, int high);
-void partition(int arr[], int low, int high);
+int partition(int arr[], int low, int high);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
