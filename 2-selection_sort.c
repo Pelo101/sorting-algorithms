ef2fdec735009272b000ead3dcbbfaebb4135464
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-02-20 06:50:04 -0800

    task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,18 +8,19 @@
  *@array: array of integers.
  *@size: size of array.
  */
+
 void selection_sort(int *array, size_t size)
 {
 
 	size_t i, j, idx;
 	int temp;
+	int num_swaps;
 
-	if (array == NULL)
+	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < (size - 1); i++)
 	{
-
 		idx = i;
 
 		for (j = i + 1; j < size; j++)
@@ -27,19 +28,18 @@ void selection_sort(int *array, size_t size)
 			if (array[j] < array[idx])
 			{
 				idx = j;
+				num_swaps = 1;
 			}
 
 		}
-		if (idx != i)
+		if (num_swaps == 1)
 		{
 			temp = array[i];
 			array[i] = array[idx];
 			array[idx] = temp;
-
+			print_array(array, size);
+			num_swaps = 0;
 		}
-		print_array(array, size);
-
-
 	}
 
 }
